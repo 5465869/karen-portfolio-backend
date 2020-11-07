5345c817fb4bbaab95f6895358a362921f49346f
5465869 <css010@uark.edu>
2020-11-07 07:35:23 -0600

    I'm doing some image stuff

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const path = require('path')
 const cors = require('cors')
+const multer = require('multer');
+const path = require('path');
+const helpers = require('./helpers');
 const PORT = process.env.PORT || 5000
 
 const { Pool } = require('pg');
@@ -11,7 +14,7 @@ const pool = new Pool({
   }
 });
 
-var app = express();
+const app = express();
 
 
 app.use(cors()) 
@@ -19,7 +22,43 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('pages/index'))
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+      cb(null, 'uploads/');
+  },
+
+  // By default, multer removes file extensions so let's add them back
+  filename: function(req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.post('/upload-profile-pic', (req, res) => {
+  // 'profile_pic' is the name of our file input field in the HTML form
+  let upload = multer({ storage: storage, fileFilter: helpers.imageFilter }).single('profile_pic');
+
+  upload(req, res, function(err) {
+      // req.file contains information of uploaded file
+      // req.body contains information of text fields, if there were any
+
+      if (req.fileValidationError) {
+          return res.send(req.fileValidationError);
+      }
+      else if (!req.file) {
+          return res.send('Please select an image to upload');
+      }
+      else if (err instanceof multer.MulterError) {
+          return res.send(err);
+      }
+      else if (err) {
+          return res.send(err);
+      }
+
+      // Display uploaded image for user validation
+      res.send(`You have uploaded this image: <hr/><img src="${req.file.path}" width="500"><hr /><a href="./">Upload another image</a>`);
+  });
+});
 
 app.get('/db', async (req, res) => {
   try {
