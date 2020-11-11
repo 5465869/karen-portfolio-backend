48893f1326fa32c94133e2bfbc8e91f608a22cd0
5465869 <css010@uark.edu>
2020-11-10 22:01:17 -0600

    adjusted to write to database

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const multer = require('multer');
 const helpers = require('./helpers');
 const PORT = process.env.PORT || 5000
 
+
+let storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+      cb(null, __dirname + '/uploads/images');
+   },
+  filename: function (req, file, cb) {
+      cb(null , file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage })
+
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -21,43 +33,17 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('pages/index'))
-const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-      cb(null, 'uploads/');
-  },
-
-  // By default, multer removes file extensions so let's add them back
-  filename: function(req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+app.post('/upload', upload.single('photo'), (req, res) => {
+  if(req.file) {
+      res.json(req.file);
+      console.log(req.body.email);
+      const client = await pool.connect();
+      const result = await client.query(`INSERT INTO test_table (image_title,image_size,image_path) VALUES (${req.body.title},${req.body.size},${req.file.path})`);
   }
+  else throw 'error';
 });
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-app.post('/upload-profile-pic', (req, res) => {
-  // 'profile_pic' is the name of our file input field in the HTML form
-  let upload = multer({ storage: storage, fileFilter: helpers.imageFilter }).single('profile_pic');
-
-  upload(req, res, function(err) {
-      // req.file contains information of uploaded file
-      // req.body contains information of text fields, if there were any
-
-      if (req.fileValidationError) {
-          return res.send(req.fileValidationError);
-      }
-      else if (!req.file) {
-          return res.send('Please select an image to upload');
-      }
-      else if (err instanceof multer.MulterError) {
-          return res.send(err);
-      }
-      else if (err) {
-          return res.send(err);
-      }
-
-      // Display uploaded image for user validation
-      res.send(`You have uploaded this image: <hr/><img src="${req.file.path}" width="500"><hr /><a href="./">Upload another image</a>`);
-  });
-});
 
 app.get('/db', async (req, res) => {
   try {
