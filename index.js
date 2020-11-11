c3d56e33004313ccf7deff7ddc85cec23e656c87
5465869 <css010@uark.edu>
2020-11-10 22:24:25 -0600

    what the frick

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 
 let storage = multer.diskStorage({
   destination: function(req, file, cb) {
-      cb(null, 'public/uploads'));
+      cb(null, 'public/uploads');
    },
   filename: function (req, file, cb) {
       cb(null , file.originalname);
