7b899902cae51d093280f25cdd135bac26862e10
5465869 <css010@uark.edu>
2020-12-03 11:31:24 -0600

    tryna upload pictures here

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const path = require('path')
 const cors = require('cors')
 const multer = require('multer');
 const PORT = process.env.PORT || 5000
+const cloudinary = require('cloudinary')
 
-
-let storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-      cb(null, 'public/uploads');
-   },
-  filename: function (req, file, cb) {
-      cb(null , file.originalname);
-  }
+cloudinary.config({ 
+  cloud_name: 'hm408jhub', 
+  api_key: '742975669727563', 
+  api_secret: 'TFmxuP7UegKQ6yyF80qgIgoq6vg' 
 });
 
-const upload = multer({ storage: storage })
+
+
+
+
 
 const { Pool } = require('pg');
 const pool = new Pool({
@@ -32,11 +32,13 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('pages/index'))
-app.post('/upload', upload.single('photo'), async (req, res) => {
+app.post('/upload', async (req, res) => {
   if(req.file) {
       res.json(req.file);
-      console.log(req.body.email);
+      
       try {
+        const fileUpload = await cloudinary.uploader.upload(req.file,
+          function(result) { console.log(result) });
         const client = await pool.connect();
         const result = await client.query(`INSERT INTO images (image_title,image_size,image_path) VALUES (${req.body.title},${req.body.size},${req.file.path})`);
         client.release();
