d4364c1d3a401a2c485ba3b92fbc888664eb3dcc
5465869 <css010@uark.edu>
2020-12-04 14:21:32 -0600

    created image get request

--- index.js
+++ index.js
@@ -82,6 +82,19 @@ app.post("/upload", (req, res) => {
   }
 });
 
+app.get('/images', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM images');
+    const results = { 'results': (result) ? result.rows : null};
+    res.json(results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
+
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
