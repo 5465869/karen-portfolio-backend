7e5b463908920e99b33cffb13daaff1b0d2ae691
5465869 <css010@uark.edu>
2020-12-06 18:18:50 -0600

    whhops

--- index.js
+++ index.js
@@ -95,18 +95,7 @@ app.get('/images', async (req, res) => {
   }
 })
 
-app.get('/images/:id', async (req, res) => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query(`SELECT * FROM images WHERE id=${req.params.id}`);
-    const results = { 'results': (result) ? result.rows : null};
-    res.json(results);
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
-})
+
 
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
