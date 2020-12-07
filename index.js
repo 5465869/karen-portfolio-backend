43ad8ae282aec80923834c4d77d8c0d5db6268d1
5465869 <css010@uark.edu>
2020-12-06 18:58:50 -0600

    change send type for route

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ app.get('/images/:id', async (req, res) => {
     const client = await pool.connect();
     const result = await client.query(`SELECT * FROM images WHERE id=${req.params.id}`);
     const results = { 'results': (result) ? result.rows : null};
-    res.json(results);
+    res.send(results);
     client.release();
   } catch (err) {
     console.error(err);
